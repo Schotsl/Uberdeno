5a4dc1dd4240f3e7db66e8feb84ce56d01060c9d
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-12 17:25:17 +0100

    ♻️ Removed poopoo code

--- entity/BaseEntity.ts
+++ entity/BaseEntity.ts
@@ -1,7 +1,6 @@
 import BareEntity from "./BareEntity.ts";
 
 export default class BaseEntity extends BareEntity {
-  // Remove 2 hours to adjust for UTC database
-  public created: Date = new Date(Date.now() - 1000 * 60 * 60 * 2);
-  public updated: Date = new Date(Date.now() - 1000 * 60 * 60 * 2);
+  public created: Date = new Date();
+  public updated: Date = new Date();
 }
