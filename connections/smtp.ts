6d296e98d82a4dda1698544718f2cf9dddcf1b49
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-12 17:25:47 +0100

    ✨ Added SMTP instance

---
+++ connections/smtp.ts
@@ -0,0 +1,22 @@
+import { SmtpClient } from "https://raw.githubusercontent.com/Schotsl/deno-smtp/master/smtp.ts";
+import { initializeEnv } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/helper.ts";
+
+initializeEnv([
+  "IMAP_PORT",
+  "IMAP_HOSTNAME",
+  "IMAP_USERNAME",
+  "IMAP_PASSWORD",
+]);
+
+const smtpClient = new SmtpClient();
+
+await smtpClient.connectTLS({
+  hostname: Deno.env.get("IMAP_HOSTNAME")!,
+  username: Deno.env.get("IMAP_USERNAME")!,
+  password: Deno.env.get("IMAP_PASSWORD")!,
+  port: +Deno.env.get("IMAP_PORT")!,
+});
+
+// TODO: Implement test SMTP function
+
+export default smtpClient;
