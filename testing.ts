57a07f4bd37cfc4478b84422052ea5797b332037
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-29 16:58:11 +0100

    🔥 Removed unused import

--- testing.ts
+++ testing.ts
@@ -1,7 +1,5 @@
 import { Client } from "https://deno.land/x/mysql@v2.10.1/mod.ts";
 
-import { gray, green, red } from "https://deno.land/std@0.110.0/fmt/colors.ts";
-
 export async function testMysql(mysqlClient: Client) {
   const port = mysqlClient.config.port!;
   const host = mysqlClient.config.hostname!;
