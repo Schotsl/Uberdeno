f193ba5d767255e3247e7f74afe0abb52709ce1a
Sjors van Holst <sjorsvanholst@gmail.com>
2021-12-10 18:41:36 +0100

    🔥 Removed old user logic

--- collection/UserCollection.ts
+++
@@ -1,6 +0,0 @@
-import BaseCollection from "./BaseCollection.ts";
-import UserEntity from "../entity/UserEntity.ts";
-
-export default class UserCollection extends BaseCollection {
-  public users: UserEntity[] = [];
-}
