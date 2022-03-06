5ebad49fae5cd891c8bf581cfa216d02893eb2db
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-06 19:02:13 +0100

    ✨ Added Discord client

---
+++ services/discordClient.ts
@@ -0,0 +1,36 @@
+import { initializeEnv } from "../helper.ts";
+import { createBot, startBot, sendMessage, Bot, CreateBotOptions, CreateMessage } from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
+
+initializeEnv([
+  "DISCORD_ID",
+  "DISCORD_TOKEN",
+]);
+
+class DiscordClient {
+  discordeno: Bot;
+
+  constructor(config: CreateBotOptions) {
+    this.discordeno = createBot(config);
+  }
+
+  async sendError(message: CreateMessage) {
+    const channel = BigInt(948935943811903540n);
+    await sendMessage(this.discordeno, channel, message);
+  }
+
+  async sendLog(message: CreateMessage) {
+    const channel = BigInt(949741041840435220n);
+    await sendMessage(this.discordeno, channel, message);
+  }
+}
+
+const discordClient = new DiscordClient({
+  token: Deno.env.get("DISCORD_TOKEN")!,
+  botId: BigInt(Deno.env.get("DISCORD_ID")!),
+  events: {},
+  intents: [],
+});
+
+await startBot(discordClient.discordeno);
+
+export default discordClient;
