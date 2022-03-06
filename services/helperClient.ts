66360f5e76dfe6b47a8c41f4338987bcd9a53552
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-06 19:01:50 +0100

    ✨ Added helper client

---
+++ services/helperClient.ts
@@ -0,0 +1,36 @@
+import { initializeEnv } from "../helper.ts";
+
+initializeEnv([
+  "HELPER_IP",
+  "HELPER_KEY",
+]);
+
+interface helperConfig {
+  helperIp: string;
+  helperKey: string;
+}
+
+class HelperClient {
+  helperIp: string;
+  helperKey: string;
+
+  constructor(config: helperConfig) {
+    this.helperIp = config.helperIp;
+    this.helperKey = config.helperKey;
+  }
+
+  async alert() {
+    const url = `http://helper.bot-ross.dev/v1/hue/alert?key=${this.helperKey}&ip=${this.helperIp}`;
+    const method = "POST";
+    const headers = { 'Content-Type': 'application/json' };
+
+    fetch(url, { method, headers }).then(response => console.log(response));
+  }
+}
+
+const helperClient = new HelperClient({
+  helperIp: Deno.env.get("HELPER_IP")!,
+  helperKey: Deno.env.get("HELPER_KEY")!,
+});
+
+export default helperClient;
