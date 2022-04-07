d5eea73754930ef11f213a334b89796ba09dea41
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-07 16:57:29 +0200

    :poop: Switching to temporary fork

--- services/smtpClient.ts
+++ services/smtpClient.ts
@@ -1,4 +1,4 @@
-import { SmtpClient } from "https://raw.githubusercontent.com/Schotsl/deno-smtp/master/smtp.ts";
+import { SmtpClient } from "https://raw.githubusercontent.com/Schotsl/denomailer/master/smtp.ts";
 import { initializeEnv } from "../helper.ts";
 
 initializeEnv([
@@ -9,7 +9,6 @@ initializeEnv([
 ]);
 
 const smtpClient = new SmtpClient();
-
 await smtpClient.connectTLS({
   hostname: Deno.env.get("IMAP_HOSTNAME")!,
   username: Deno.env.get("IMAP_USERNAME")!,
