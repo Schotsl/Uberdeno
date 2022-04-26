a6353c11a117bfc492050e842e24527ddfe6c203
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-26 16:24:06 +0200

    🎨 Formatted codebase

--- other/Server.ts
+++ other/Server.ts
@@ -1,15 +1,22 @@
 // deno-lint-ignore-file no-explicit-any
 
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import { Application, Middleware, State, Context } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 import {
-  errorHandler,
-  limitHandler,
-  postHandler,
-} from "../middleware.ts";
+  Application,
+  Context,
+  Middleware,
+  State,
+} from "https://deno.land/x/oak@v10.5.1/mod.ts";
+import { errorHandler, limitHandler, postHandler } from "../middleware.ts";
 
-const id = Deno.env.get("DENO_DEPLOYMENT_ID") ? Deno.env.get("DENO_DEPLOYMENT_ID") : false;
-const region = Deno.env.get("DENO_DEPLOYMENT_ID") ? Deno.env.get("DENO_DEPLOYMENT_ID") : false;
+const id = Deno.env.get("DENO_DEPLOYMENT_ID")
+  ? Deno.env.get("DENO_DEPLOYMENT_ID")
+  : false;
+
+const region = Deno.env.get("DENO_DEPLOYMENT_ID")
+  ? Deno.env.get("DENO_DEPLOYMENT_ID")
+  : false;
+  
 const version = "v1.0.0";
 
 export default class Server {
@@ -24,17 +31,19 @@ export default class Server {
     this.application.use(limitHandler);
     this.application.use(postHandler);
 
-    this.application.use(( { response }) => {
+    this.application.use(({ response }) => {
       response.body = {
         id,
         region,
         version,
-      }
+      };
     });
   }
 
-  public use(middleware: Middleware<State, Context<State, Record<string, any>>>) {
-    return this.application.use(middleware );
+  public use(
+    middleware: Middleware<State, Context<State, Record<string, any>>>,
+  ) {
+    return this.application.use(middleware);
   }
 
   public listen(port = 8080) {
