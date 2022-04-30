3ffda02e36f85932a0ac5c70febfe1c1f0762d50
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-30 14:08:12 +0200

    :bug: Fixed property

--- other/Server.ts
+++ other/Server.ts
@@ -14,8 +14,8 @@ const id = Deno.env.get("DENO_DEPLOYMENT_ID")
   ? Deno.env.get("DENO_DEPLOYMENT_ID")
   : false;
 
-const region = Deno.env.get("DENO_DEPLOYMENT_ID")
-  ? Deno.env.get("DENO_DEPLOYMENT_ID")
+const region = Deno.env.get("DENO_REGION")
+  ? Deno.env.get("DENO_REGION")
   : false;
 
 const version = "v1.0.0";
