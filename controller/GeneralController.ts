ba9e211a4ecbbaed58e512313ef6461ff4654390
GitHub <noreply@github.com>
2022-05-24 00:11:39 +0000

    ⬆️ Update dependencies

--- controller/GeneralController.ts
+++ controller/GeneralController.ts
@@ -5,7 +5,7 @@ import {
   Request,
   Response,
   State,
-} from "https://deno.land/x/oak@v10.5.1/mod.ts";
+} from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import { generateColumns, populateInstance, renderREST } from "../helper.ts";
 
 import BaseEntity from "../entity/BaseEntity.ts";
