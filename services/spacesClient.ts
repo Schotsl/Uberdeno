c94d519a60ba37fe45c430263d909c09a9e6f8a0
Sjors van Holst <sjorsvanholst@gmail.com>
2022-07-16 13:24:07 +0200

    ✨ Implemented S3 spaces client

--- services/spacesClient.ts
+++ services/spacesClient.ts
@@ -1,5 +1,5 @@
-import { S3Bucket, S3BucketConfig } from "https://deno.land/x/s3@0.5.0/mod.ts";
 import { initializeEnv } from "../helper.ts";
+import { getSignedUrl } from 'https://deno.land/x/aws_s3_presign/mod.ts'
 
 initializeEnv([
   "SPACES_ID",
@@ -9,54 +9,32 @@ initializeEnv([
   "SPACES_ENDPOINT",
 ]);
 
+const spacesCredentials = {
+  region: Deno.env.get("SPACES_REGION")!,
+  endpoint: Deno.env.get("SPACES_ENDPOINT")!,
+  bucketName: Deno.env.get("SPACES_BUCKET")!,
+  accessKeyId: Deno.env.get("SPACES_ID")!,
+  secretAccessKey: Deno.env.get("SPACES_KEY")!,
+}
+
 class SpacesClient {
-  bucket: S3Bucket;
-
-  constructor(config: S3BucketConfig) {
-    this.bucket = new S3Bucket(config);
+  signedGET(filename: string) {
+    return getSignedUrl({
+      method: 'GET',
+      objectPath: `/${filename}`,
+      ...spacesCredentials,
+    })
   }
 
-  async deleteBase64(name: string) {
-    await this.bucket.deleteObject(name);
-  }
-
-  async signedGET(file: string) {
-    const headers = { "Content-Type": "application/json" };
-    const method = "POST";
-    const body = JSON.stringify({ file });
-
-    const response = await fetch("http://localhost:69/v1/signed/get", {
-      headers,
-      method,
-      body,
-    });
-    const parsed = await response.text();
-
-    return parsed;
-  }
-
-  async signedPUT(file: string, type: string, size: number) {
-    const headers = { "Content-Type": "application/json" };
-    const method = "POST";
-    const body = JSON.stringify({ file, type, size });
-
-    const response = await fetch("http://localhost:69/v1/signed/put", {
-      headers,
-      method,
-      body,
-    });
-    const parsed = await response.text();
-
-    return parsed;
+  signedPUT(filename: string) {
+    return getSignedUrl({
+      method: 'PUT',
+      objectPath: `/${filename}`,
+      ...spacesCredentials,
+    })
   }
 }
 
-const spacesClient = new SpacesClient({
-  endpointURL: Deno.env.get("SPACES_ENDPOINT")!,
-  accessKeyID: Deno.env.get("SPACES_ID")!,
-  secretKey: Deno.env.get("SPACES_KEY")!,
-  region: Deno.env.get("SPACES_REGION")!,
-  bucket: Deno.env.get("SPACES_BUCKET")!,
-});
+const spacesClient = new SpacesClient();
 
 export default spacesClient;
