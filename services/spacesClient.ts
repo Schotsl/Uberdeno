521ba98aa68a5b44da732f2d3a3faf5f34df7c7c
Sjors van Holst <sjorsvanholst@gmail.com>
2022-07-23 13:28:54 +0200

    🎨 Formatted codebase

--- services/spacesClient.ts
+++ services/spacesClient.ts
@@ -1,5 +1,5 @@
 import { initializeEnv } from "../helper.ts";
-import { getSignedUrl } from 'https://deno.land/x/aws_s3_presign/mod.ts'
+import { getSignedUrl } from "https://deno.land/x/aws_s3_presign/mod.ts";
 
 initializeEnv([
   "SPACES_ID",
@@ -15,23 +15,23 @@ const spacesCredentials = {
   bucketName: Deno.env.get("SPACES_BUCKET")!,
   accessKeyId: Deno.env.get("SPACES_ID")!,
   secretAccessKey: Deno.env.get("SPACES_KEY")!,
-}
+};
 
 class SpacesClient {
   signedGET(filename: string) {
     return getSignedUrl({
-      method: 'GET',
+      method: "GET",
       objectPath: `/${filename}`,
       ...spacesCredentials,
-    })
+    });
   }
 
   signedPUT(filename: string) {
     return getSignedUrl({
-      method: 'PUT',
+      method: "PUT",
       objectPath: `/${filename}`,
       ...spacesCredentials,
-    })
+    });
   }
 }
 
