c139bb1f5e1e74a16c82dc39f8982bc1d5b0a9dc
Sjors van Holst <sjorsvanholst@gmail.com>
2022-10-14 13:33:38 +0200

    ✨ Added S3 delete

--- services/spacesClient.ts
+++ services/spacesClient.ts
@@ -1,5 +1,6 @@
+import { S3Bucket } from "https://deno.land/x/s3@0.4.1/mod.ts";
+import { getSignedUrl } from "https://deno.land/x/aws_s3_presign@1.3.0/mod.ts";
 import { initializeEnv } from "../helper.ts";
-import { getSignedUrl } from "https://deno.land/x/aws_s3_presign/mod.ts";
 
 initializeEnv([
   "SPACES_ID",
@@ -9,7 +10,7 @@ initializeEnv([
   "SPACES_ENDPOINT",
 ]);
 
-const spacesCredentials = {
+const signedCredentials = {
   region: Deno.env.get("SPACES_REGION")!,
   endpoint: Deno.env.get("SPACES_ENDPOINT")!,
   bucketName: Deno.env.get("SPACES_BUCKET")!,
@@ -17,12 +18,26 @@ const spacesCredentials = {
   secretAccessKey: Deno.env.get("SPACES_KEY")!,
 };
 
+const spacesCredentials = {
+  bucket: Deno.env.get("SPACES_BUCKET")!,
+  region: Deno.env.get("SPACES_REGION")!,
+  secretKey: Deno.env.get("SPACES_KEY")!,
+  accessKeyID: Deno.env.get("SPACES_ID")!,
+  endpointURL: Deno.env.get("SPACES_ENDPOINT")!,
+};
+
 class SpacesClient {
+  bucket: S3Bucket;
+
+  constructor() {
+    this.bucket = new S3Bucket(spacesCredentials);
+  }
+
   signedGET(filename: string) {
     return getSignedUrl({
       method: "GET",
       objectPath: `/${filename}`,
-      ...spacesCredentials,
+      ...signedCredentials,
     });
   }
 
@@ -30,8 +45,12 @@ class SpacesClient {
     return getSignedUrl({
       method: "PUT",
       objectPath: `/${filename}`,
-      ...spacesCredentials,
+      ...signedCredentials,
     });
+  }
+
+  deleteFile(filename: string) {
+    return this.bucket.deleteObject(filename);
   }
 }
 
